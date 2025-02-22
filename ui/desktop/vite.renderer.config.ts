2f94563d36fd7f9fc25f1ba968a9d41100a9d60a
lily-de <119957291+lily-de@users.noreply.github.com>
2025-02-22 14:58:52 -0500

    Update comment in Vite config file

--- ui/desktop/vite.renderer.config.ts
+++ ui/desktop/vite.renderer.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
-    // ...any existing config you may have...
     define: {
-        // This replaces process.env.ALPHA in your React code at build time
+        // This replaces process.env.ALPHA with a literal at build time
         'process.env.ALPHA': JSON.stringify(process.env.ALPHA === 'true'),
     },
 });
